3d147aa2a21e312308e155611ba0edc97cd128de
Haejun Jang <codingjun@naver.com>
2025-04-03 22:56:14 +0900

    입문 3일차

---
+++ src/programmers/Day3_4.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class Day3_4 {
+    public int[] solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i <=n; i++) {
+            if ( i%2 ==1) {
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
