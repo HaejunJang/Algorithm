b81d4cfef96c40e909f0f04306ec14577ad62ba6
Haejun Jang <codingjun@naver.com>
2025-04-07 23:00:26 +0900

    Day9

---
+++ src/programmers/lv0/Day9_2.java
@@ -0,0 +1,9 @@
+package programmers.lv0;
+
+public class Day9_2 {
+    class Solution {
+        public int solution(int hp) {
+            return hp/5 + (hp%5 /3) + (hp %5 %3);
+        }
+    }
+}
