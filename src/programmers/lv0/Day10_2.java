1f1b0423d86caf6aa02d71269b259ebfe6dc0ef9
Haejun Jang <codingjun@naver.com>
2025-04-09 21:53:24 +0900

    Day10

---
+++ src/programmers/lv0/Day10_2.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+
+public class Day10_2 {
+    class Solution {
+        public int[][] solution(int[] num_list, int n) {
+            int[][] answer = new int[num_list.length/n][n]; //[4][2]. num_list.length = 8
+            int count =0;
+            for (int i=0; i<num_list.length/n; i++) {
+                for (int j=0; j<n; j++) {
+                    answer[i][j] = num_list[count];
+                    count++;
+                }
+            }
+            return answer;
+        }
+    }
+}
