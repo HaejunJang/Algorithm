2562951ba4d78cddd9cd26655d88b02252421f24
Haejun Jang <codingjun@naver.com>
2025-04-15 20:56:40 +0900

    Day13

---
+++ src/programmers/lv0/Day13_3.java
@@ -0,0 +1,16 @@
+package programmers.lv0;
+
+public class Day13_3 {
+    class Solution {
+        public String solution(String cipher, int code) {
+            String answer = "";
+            int i =1;
+            while (true) {
+                if(code * i >cipher.length()) break;
+                answer += String.valueOf(cipher.charAt(code * i -1));
+                i++;
+            }
+            return answer;
+        }
+    }
+}
