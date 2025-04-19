88a74d2efd334e9d25a41e2e4ed875c71611e0a9
Haejun Jang <codingjun@naver.com>
2025-04-19 18:08:37 +0900

    Day17

---
+++ src/programmers/lv0/Day17_2.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+import java.util.*;
+
+public class Day17_2 {
+    class Solution {
+        public String solution(String my_string) {
+            String answer = "";
+            my_string = my_string.toLowerCase();
+            String[] arr = my_string.split("");
+            Arrays.sort(arr);
+            for(int i=0; i<arr.length; i++) {
+                answer += arr[i];
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/programmers/lv0/Day17_4.java
@@ -0,0 +1,20 @@
+package programmers.lv0;
+import java.util.*;
+
+public class Day17_4 {
+    class Solution {
+        public int solution(String before, String after) {
+            int answer = 1;
+            String[] beArr = before.split("");
+            String[] afArr = after.split("");
+
+            Arrays.sort(beArr);
+            Arrays.sort(afArr);
+
+            for(int i=0; i<beArr.length; i++) {
+                if(!beArr[i].equals(afArr[i])) return 0;
+            }
+            return answer;
+        }
+    }
+}
