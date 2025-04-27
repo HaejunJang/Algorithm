0b3d821ad163d000c935df76f060005801075599
Haejun Jang <codingjun@naver.com>
2025-04-28 00:22:47 +0900

    Day22

---
+++ src/programmers/lv0/Day22_2.java
@@ -0,0 +1,31 @@
+package programmers.lv0;
+
+public class Day22_2 {
+    class Solution {
+        public int solution(String[] babbling) {
+            int answer = 0;
+            for( int i=0; i<babbling.length; i++) {
+
+                while(babbling.length>0) {
+                    if (babbling[i].startsWith("aya")) {
+                        babbling[i]= babbling[i].substring(3);
+                        System.out.println("aya 문 실행");
+                    }else if ( babbling[i].startsWith("ye")) {
+                        babbling[i] = babbling[i].substring(2);
+                    } else if (babbling[i].startsWith("woo")) {
+                        babbling[i] = babbling[i].substring(3);
+                    } else if (babbling[i].startsWith("ma")) {
+                        babbling[i] = babbling[i].substring(2);
+                    } else {
+                        System.out.println(i +"번째 끝나고 길이 체크 : " + babbling[i].length());
+                        break;
+                    }
+                }
+                if(babbling[i].length() == 0) {
+                    answer++;
+                }
+            }
+            return answer;
+        }
+    }
+}
---
+++ src/programmers/lv0/Day22_3.java
@@ -0,0 +1,19 @@
+package programmers.lv0;
+
+public class Day22_3 {
+    class Solution {
+        public String solution(String[] id_pw, String[][] db) {
+            String answer = "fail";
+            for(int i=0; i<db.length; i++) {
+                if(db[i][0].equals(id_pw[0])) {
+                    if(db[i][1].equals(id_pw[1])) {
+                        answer = "login";
+                    }else {
+                        answer = "wrong pw";
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
