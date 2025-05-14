f7096dbb11fbc10ab973cafa4fcccb7626eed85d
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-05-14 16:34:01 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
