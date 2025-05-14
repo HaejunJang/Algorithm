ea18efb4ef9786ada5d663635a203a19a91c3a70
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-05-14 20:45:18 +0900

    [Bronze IV] Title: $A + B - C$, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String temp ="";
+        System.out.println(a + b - c);
+        temp = String.valueOf(a) + String.valueOf(b);
+        System.out.println(Integer.parseInt(temp) - c);
+    }
+}
