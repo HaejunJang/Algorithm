f86a24dd3d8c8c77ea34188d236e5d94b8f561da
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-05-15 14:01:19 +0900

    [Bronze III] Title: 최소, 최대, Time: 2288 ms, Memory: 320360 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(); //계산 수
+        int[] arr = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[arr.length - 1]);
+    }
+}
