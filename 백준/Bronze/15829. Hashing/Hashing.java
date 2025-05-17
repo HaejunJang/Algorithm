53ec91afbc15a4c17ffd323818b0cc03ae05a49a
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-05-17 18:52:01 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17676 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = Integer.parseInt(sc.nextLine());
-        int sum = 0;
+        int m = 1234567891;
+        long sum = 0;
+        long power = 1;
         String input = sc.nextLine();
-        for(int i=0; i<input.length(); i++) {
-            sum += (input.charAt(i) -'a' +1) * Math.pow(31,i);
+        for(int i=0; i<count; i++) {
+            int value = input.charAt(i) - 'a'+1;
+            sum = (sum + value * power) %m;
+//            sum += value * power % m;
+            power = (power * 31) %m;
         }
         System.out.println(sum);
     }
