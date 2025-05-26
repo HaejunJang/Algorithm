db886692ec418c4b3cb0e62a137b132dab492535
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-05-26 13:26:38 +0900

    [Silver IV] Title: 제로, Time: 244 ms, Memory: 24156 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count =Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+        for (int i=0; i<count; i++) {
+            int number = Integer.parseInt(br.readLine());
+            if (number == 0) {
+                stack.pop();
+            } else {
+                stack.push(number);
+            }
+        }
+        int sum =0;
+        while(!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+    }
+}
