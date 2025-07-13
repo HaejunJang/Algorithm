0daea26b9944564cf87a2892bbc27442f1ffe2a1
haejun <162435572+HaejunJang@users.noreply.github.com>
2025-07-13 23:28:39 +0900

    [level 1] Title: 실패율, Time: 9.52 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        //스테이지별 도전자 수를 구하기
+        int[] challenger = new int[N+2];
+        for (int i=0; i<stages.length; i++) {
+            challenger[stages[i]] +=1;
+        }
+        //스테이지별 실패한 사용자 수 계산
+        HashMap<Integer, Double> fails = new HashMap<>();
+        double total = stages.length;
+        
+        //실패율 계산
+        for (int i=1; i<=N; i++) {
+            if (challenger[i] == 0) {
+                fails.put(i, 0.0);
+            } else {
+                fails.put(i, challenger[i]/total);
+                total -= challenger[i];
+            }
+        }
+        return  fails.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
+    }
+}
